587f8f3fa1af86f8ac616d7dfbe5cb521708fc38
Jamon Terrell <jamon@sofea.net>
2011-12-03 13:31:33 -0500

    added if statement, printing, etc. added a few TODOs

--- compile.js
+++ compile.js
@@ -27,14 +27,69 @@ SoyCompiler.prototype.parse = function() {
     this.parsed = parser.parse(this.soy);
 };
 SoyCompiler.prototype.compile = function() {
+    // @TODO add namespace handlers (to allow for AMD/commonJS loader, goog.load, or namespaced globals, etc)
+    var p = this.parsed;
     this
         ._("/**").nl()
         ._(" * Automatically generated from soy template").nl()
+        ._(" *  -- ", p.namespace).nl()
         ._(" */").nl()
-        ._("var ", this.parsed.namespace, " = function() {").i()
-            ._("// test").u()
-        ._("}");
+        ._("var template  = function() {};").nl()
+        .compileTemplates(p);
     return this._code.join("");
+};
+SoyCompiler.prototype.compileTemplates = function(p) {
+    for(var i = 0; i < p.templates.length; i++) {
+        this.nl().compileTemplate(p.templates[i]);
+    }
+};
+SoyCompiler.prototype.compileTemplate = function(t) {
+    this
+        ._("/**").nl()
+        ._(" * ", t.description).nl()
+        ._(" */").nl()
+        ._("template.", t.name, " = function(o) {").i()
+            ._("var output = [];").nl()
+            .compileContent(t.content).u()
+        ._("};");
+    return this;
+};
+SoyCompiler.prototype.compileContent = function(c) {
+    for(var i = 0; i < c.length; i++) {
+        if(i > 0) this.nl();
+        this.compileCommand(c[i]);
+    };
+    return this;
+};
+SoyCompiler.prototype.compileCommand = function(c) {
+    switch(c.command) {
+        case "print_text":
+            // @todo add output buffering on print statements, to combine multiples
+            this._('output.push("', c.content, '");');
+            break;
+        case "if":
+            // @TODO add ternary when appropriate
+            // if statement
+            this._('if (').compileExpression(c.conditions[0].expression)._(') {').i()
+                .compileContent(c.conditions[0].content).u();
+            for(var i = 1; i < c.conditions.length; i++) {
+                this._('} else if (').compileExpression(c.conditions[i].expression)._(') {').i()
+                    .compileContent(c.conditions[i].content).u();
+            }
+            if(c.else_content !== "") {
+                this._('} else {').i()
+                    .compileContent(c.else_content).u();
+            }
+            this._('}');
+            break;
+        default:
+            break;
+    }
+    return this;
+};
+SoyCompiler.prototype.compileExpression = function(e) {
+    this.code(e);
+    return this;
 };
 SoyCompiler.prototype.indent = function(omitNewline) {
     this._indentCount++;
@@ -48,7 +103,7 @@ SoyCompiler.prototype.unindent = function(omitNewline) {
 };
 SoyCompiler.prototype.u = SoyCompiler.prototype.unindent; // alias
 SoyCompiler.prototype.nl = function() {
-    return this.code(this.newline);
+    return this.code(this.newline, this._indent);
 };
 SoyCompiler.prototype.calculateIndent = function() {
     return this._indent = new Array(this._indentCount + 1).join(this.indentString);
